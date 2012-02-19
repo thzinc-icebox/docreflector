ad71ae40811e9fb98b50109246df29915aeb0ce0
thzinc <thzinc@c365c90f-653b-4e4a-9251-f02d6232ff27>
2012-02-19 16:24:56 +0000

    Updated copyright

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("djames")]
+[assembly: AssemblyCopyright("Daniel James 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
